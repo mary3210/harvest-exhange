4ff13e88b6a268d1d100ff9dcad8e7a2fe5f5c33
Mary Rodriguez <maryrodri3210@gmail.com>
2024-01-11 16:19:45 -0600

    Deleting Node modules from Base

--- server.js
+++ server.js
@@ -57,10 +57,10 @@ app.post("/auth", async (req, res) => {
   }
 });
 
-app.use(express.static('../frontend/build'));
+app.use(express.static('./frontend/build'));
 const path = require('path');
 app.get('*', (req, res) => { 
-  res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './frontend', 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
